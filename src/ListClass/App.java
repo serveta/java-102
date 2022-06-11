a2b458192c84d23becdf65ee3789f31693166054
Servet Arslan <arslannservett@gmail.com>
2022-06-11 10:06:48 +0300

    ListClass completed

--- src/ListClass/App.java
+++ src/ListClass/App.java
@@ -2,6 +2,78 @@ package ListClass;
 
 public class App {
     public static void main(String[] args) {
+        MyList<Integer> listInt = new MyList<>();
+
+        System.out.println("Dizideki elemanlar : " + listInt.toString());
+        System.out.println("Dizideki Eleman Sayısı : " + listInt.size());
+        System.out.println("Dizinin Kapasitesi : " + listInt.getCapacity());
+        System.out.println();
+        listInt.add(1);
+        listInt.add(2);
+        listInt.add(3);
+        listInt.add(4);
+        listInt.add(5);
+        listInt.add(6);
+        listInt.add(7);
+        listInt.add(8);
+        System.out.println("Dizideki elemanlar : ");
+        System.out.println(listInt.toString());
+        System.out.println("Dizideki Eleman Sayısı : " + listInt.size());
+        System.out.println("Dizinin Kapasitesi : " + listInt.getCapacity());
+        System.out.println();
+        listInt.add(9);
+        listInt.add(10);
+        listInt.add(11);
+        listInt.add(100);
+        listInt.add(13);
+        listInt.add(14);
+        System.out.println("Dizideki elemanlar : ");
+        System.out.println(listInt.toString());
+        System.out.println("Dizideki Eleman Sayısı : " + listInt.size());
+        System.out.println("Dizinin Kapasitesi : " + listInt.getCapacity());
+        System.out.println();
+
+        System.out.println("0. indiste bulunan değer: " + listInt.get(0));
+        System.out.println("3. indiste bulunan değer: " + listInt.get(3));
+
+        listInt.remove(0);
+        System.out.println("0. indis silindikten sonra dizinin yeni hali: ");
+        System.out.println(listInt.toString());
+        listInt.set(0, 100);
+        System.out.println("0. indis 100 olarak değiştirildikten sonra dizinin yeni hali: ");
+        System.out.println(listInt.toString());
+        System.out.println();
+
+        System.out.println("Liste INT Durumu : " + (listInt.isEmpty() ? "Boş" : "Dolu"));
+
+        // New list string
+        MyList<String> listStr = new MyList<>(3);
+
+        System.out.println("Liste STR Durumu : " + (listStr.isEmpty() ? "Boş" : "Dolu"));
+
+        System.out.println();
+
+        System.out.println("IndexOf(100) : " + listInt.indexOf(100));
+        System.out.println("IndexOf(111) :" + listInt.indexOf(111));
+        System.out.println("lastIndexOf(100) : " + listInt.lastIndexOf(100));
+        System.out.println();
+
+        Object[] dizi = listInt.toArray();
+        System.out.println("Object dizisinin ilk elemanı :" + dizi[0]);
+        System.out.println();
+
+        System.out.println("subList(0,3):");
+        MyList<Integer> subList = listInt.sublist(0, 3);
+        System.out.println(subList.toString());
+        System.out.println();
+
+        System.out.println("Listemde 100 değeri : " + listInt.contains(100));
+        System.out.println("Listemde 120 değeri : " + listInt.contains(120));
+        System.out.println();
+
+        listInt.clear();
+        System.out.println("Liste temizlendikten sonra listenin son hali: ");
+        System.out.println(listInt.toString());
 
     }
 }
--- src/ListClass/MyList.java
+++ src/ListClass/MyList.java
@@ -1,4 +1,155 @@
 package ListClass;
 
-public class MyList {
+public class MyList<T> {
+    private Object[] arr;
+    private Object[] newArr;
+    //private int count = 0;
+
+    public MyList() {
+        this.arr = new Object[10];
+    }
+
+    public MyList(int capacity) {
+        this.arr = new Object[capacity];
+    }
+
+    public int size() {
+        int count = 0;
+        for (Object i : this.arr) {
+            if (i != null) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public int getCapacity() {
+        return this.arr.length;
+    }
+
+    public void add(T data) {
+        if (size() == getCapacity()) {
+            int newCapacity = getCapacity() * 2;
+            this.newArr = new Object[newCapacity];
+
+            for (int i = 0; i < getCapacity(); i++) {
+                this.newArr[i] = this.arr[i];
+            }
+
+            this.arr = this.newArr;
+        }
+
+        this.arr[size()] = data;
+    }
+
+    public Object get(int index) {
+        if (index < 0 || index >= getCapacity()) {
+            return null;
+        }
+
+        return this.arr[index];
+    }
+
+    public void remove(int index) {
+        if (index < 0 || index >= getCapacity()) {
+            System.out.println("ERROR: Remove operation failed!");
+            return;
+        }
+
+        this.newArr = new Object[getCapacity()];
+
+        for (int i = 0; i < getCapacity(); i++) {
+            if (i >= index && i <= size()) {
+                this.newArr[i] = this.arr[++i];
+                i--;
+            } else {
+                this.newArr[i] = this.arr[i];
+            }
+        }
+        this.arr = this.newArr;
+    }
+
+    public void set(int index, T data) {
+        if (index < 0 || index >= getCapacity()) {
+            System.out.println("ERROR: Wrong index value!");
+            return;
+        }
+        this.arr[index] = data;
+    }
+
+    public String toString() {
+        if (this.size() > 0) {
+            System.out.print("[");
+            for (int i = 0; i <= size(); i++) {
+                if (++i == size()) {
+                    System.out.print(this.arr[--i].toString());
+                    System.out.print("]");
+                    return "";
+                } else {
+                    System.out.print(this.arr[--i].toString() + ", ");
+                }
+            }
+        }
+
+        return "[]";
+    }
+
+    public int indexOf(T data) {
+        for (int i = 0; i < size(); i++) {
+            if (this.arr[i].toString().equals(data.toString())) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public int lastIndexOf(T data) {
+        for (int i = size() - 1; i >= 0; i--) {
+            if (this.arr[i].toString().equals(data.toString())) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public boolean isEmpty() {
+        return size() == 0;
+    }
+
+    public Object[] toArray() {
+        return this.arr;
+    }
+
+    public void clear() {
+        for (int i = 0; i < getCapacity(); i++) {
+            set(i, null);
+        }
+    }
+
+    public MyList<T> sublist(int start, int finish) {
+        if (start < 0 || finish > size() || (start > finish)) {
+            System.out.println("ERROR: Out of bound!");
+            return null;
+        }
+
+        int count = 0;
+        this.newArr = new Object[getCapacity()];
+
+        for (int i = start; i <= finish; i++) {
+            this.newArr[count++] = this.arr[i];
+        }
+
+        this.arr = this.newArr;
+
+        return this;
+    }
+
+    public boolean contains(T data) {
+        for (Object val : this.arr) {
+            if (val == data) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
