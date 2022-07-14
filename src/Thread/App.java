7ddc48aa0518de5ab42e28ed896da42e461de32d
Servet Arslan <arslannservett@gmail.com>
2022-07-14 14:58:01 +0300

    Basic thread example

---
+++ src/Thread/App.java
@@ -0,0 +1,11 @@
+package Thread;
+
+public class App {
+    public static void main(String[] args) {
+        Counter counter1 = new Counter("C-1");
+        Counter counter2 = new Counter("C-2");
+
+        counter1.start();
+        counter2.start();
+    }
+}
